e39595f4099791c28a297ce4444b0af5b1b551dd
ktvprasaad <47123069+ktvprasaad@users.noreply.github.com>
2020-06-05 15:59:23 -0400

    Logkitty update
    Logkitty update and removed unused components and added comments.

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
-import { createAppContainer }  from 'react-navigation';
+import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import KeyboardSpacer from 'react-native-keyboard-spacer';
-// import { StyleSheet, Text, View, Alert, Button, TextInput } from 'react-native';
-// import the screens we want to navigate
+/** import KeyboardSpacer from 'react-native-keyboard-spacer'; */
+/**
+ * import the screens which we want to navigate
+ */
 import Start from './components/Start';
 import Chat from './components/Chat';
 
-// Order of the screen stacked Start to Chat
+/**
+ * Order of the screen stacked Start to Chat
+ */
 const navigator = createStackNavigator({
   Start: { screen: Start },
-  Chat: { screen: Chat }
+  Chat: { screen: Chat },
 });
 
 const navigatorContainer = createAppContainer(navigator);
-// Export it as the root component
+/**
+ * Export it as the root component
+ */
 export default navigatorContainer;
